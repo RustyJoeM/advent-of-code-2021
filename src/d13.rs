94b6d65b78e2cf0d864a9d306a4f5f3daca05fa8
Jozef Miklos <rusty.joe.mik@gmail.com>
2021-12-13 22:19:11 +0100

    day 13 cleanup & speedup

--- src/d13.rs
+++ src/d13.rs
@@ -1,15 +1,15 @@
-use std::{borrow::BorrowMut, collections::HashSet};
+use std::collections::BTreeSet;
 
 mod utils;
 const DAY_ID: utils::DayIdType = 13;
 
 #[derive(Debug, Copy, Clone)]
 enum Fold {
-    X(usize),
-    Y(usize),
+    Row(usize),
+    Column(usize),
 }
 
-type DotsT = HashSet<(usize, usize)>;
+type DotsT = BTreeSet<(usize, usize)>;
 
 #[derive(Debug, Clone)]
 struct TransparentPaper {
@@ -18,33 +18,63 @@ struct TransparentPaper {
     cols: usize,
 }
 
+fn parse_input(data: &str) -> (TransparentPaper, Vec<Fold>) {
+    let mut dots = BTreeSet::new();
+    let mut cols = 0;
+    let mut rows = 0;
+    let mut folds: Vec<Fold> = Vec::new();
+
+    for line in data.lines() {
+        if line.contains(',') {
+            let (x, y) = line.split_once(',').unwrap();
+            let x = x.parse::<usize>().unwrap();
+            let y = y.parse::<usize>().unwrap();
+            cols = cols.max(x);
+            rows = rows.max(y);
+            dots.insert((x, y));
+        } else if line.contains('=') {
+            let v = line.split('=').last().unwrap().parse::<usize>().unwrap();
+            folds.push(if line.contains("x=") {
+                Fold::Column(v)
+            } else {
+                Fold::Row(v)
+            });
+        }
+    }
+
+    (TransparentPaper { dots, rows, cols }, folds)
+}
+
 impl TransparentPaper {
-    fn fold(&mut self, fold: &Fold) {
-        let dots = self.dots.borrow_mut();
-        match *fold {
-            Fold::X(s) => {
-                for r in 0..=self.rows {
-                    for c in s + 1..=self.cols {
-                        dots.remove(&(r, s)); // drop line elem
-                        if dots.contains(&(r, c)) {
-                            dots.remove(&(r, c)); // drop item being folded
-                            dots.insert((r, s - (c - s))); // add folded copy
+    fn fold(&mut self, folds: &[Fold]) {
+        for fold in folds.iter() {
+            let mut next_dots = BTreeSet::new();
+            for &(x, y) in self.dots.iter() {
+                match *fold {
+                    Fold::Column(f) => {
+                        if x > f {
+                            next_dots.insert((f - (x - f), y));
+                        } else if x < f {
+                            next_dots.insert((x, y));
+                        }
+                    }
+                    Fold::Row(f) => {
+                        if y > f {
+                            next_dots.insert((x, f - (y - f)));
+                        } else if y < f {
+                            next_dots.insert((x, y));
                         }
                     }
                 }
-                self.cols = s;
             }
-            Fold::Y(s) => {
-                for r in s + 1..=self.rows {
-                    for c in 0..=self.cols {
-                        dots.remove(&(s, c)); // drop line elem
-                        if dots.contains(&(r, c)) {
-                            dots.remove(&(r, c)); // drop item being folded
-                            dots.insert((s - (r - s), c)); // add folded copy
-                        }
-                    }
+            self.dots = next_dots;
+            match *fold {
+                Fold::Column(f) => {
+                    self.cols = f - 1;
                 }
-                self.rows = s;
+                Fold::Row(f) => {
+                    self.rows = f - 1;
+                }
             }
         }
     }
@@ -54,7 +84,7 @@ impl TransparentPaper {
         for r in 0..=self.rows {
             let mut str = String::new();
             for c in 0..=self.cols {
-                if self.dots.contains(&(r, c)) {
+                if self.dots.contains(&(c, r)) {
                     str.push('\u{2588}');
                 } else {
                     str.push('.');
@@ -64,59 +94,26 @@ impl TransparentPaper {
         }
         strs
     }
-}
 
-fn parse_input(data: &str) -> (TransparentPaper, Vec<Fold>) {
-    let mut dots: DotsT = HashSet::new();
-    let mut folds: Vec<Fold> = Vec::new();
-    let mut cols = 0;
-    let mut rows = 0;
-
-    for line in data.lines() {
-        if line.contains(',') {
-            let p: Vec<usize> = line
-                .split(',')
-                .map(|x| x.parse::<usize>().unwrap())
-                .collect();
-            cols = cols.max(p[0]);
-            rows = rows.max(p[1]);
-            dots.insert((p[1], p[0]));
-        }
-        if line.contains('=') {
-            let v = line.split('=').last().unwrap().parse::<usize>().unwrap();
-            folds.push(if line.contains("x=") {
-                Fold::X(v)
-            } else {
-                Fold::Y(v)
-            });
+    fn debug_print(&self) {
+        for line in self.to_nice_strings().iter() {
+            println!("{}", line);
         }
     }
-
-    (TransparentPaper { dots, rows, cols }, folds)
 }
 
-fn fold_dots(paper: &mut TransparentPaper, folds: &[Fold]) {
-    for fold in folds.iter() {
-        paper.fold(fold);
-    }
-}
-
-fn solve_part1(input: &(TransparentPaper, Vec<Fold>)) -> usize {
-    let (paper, folds) = input;
+fn solve_part1((paper, folds): &(TransparentPaper, Vec<Fold>)) -> usize {
     let mut paper = paper.clone();
-    fold_dots(&mut paper, &folds[..1]);
+    paper.fold(&folds[..1]);
     paper.dots.len()
 }
 
-fn solve_part2(input: &(TransparentPaper, Vec<Fold>)) -> usize {
-    let (paper, folds) = input;
+fn solve_part2((paper, folds): &(TransparentPaper, Vec<Fold>)) -> usize {
     let mut paper = paper.clone();
-    fold_dots(&mut paper, folds);
+    paper.fold(folds);
 
     println!("\n-------- Part 2 output --------");
-    for line in paper.to_nice_strings().iter() {
-        println!("{}", line);
-    }
+    paper.debug_print();
     0
 }
 
