b0462e714ff766075448219758adf6db7d6a6cc0
Jozef Miklos <rusty.joe.mik@gmail.com>
2021-12-14 17:32:59 +0100

    cosmetic day 14 cleanup

--- src/d14.rs
+++ src/d14.rs
@@ -13,7 +13,7 @@ fn parse_input(data: &str) -> (Vec<char>, Transitions) {
     let x = iter.next().unwrap();
     let starting = x.chars().collect();
 
-    let mut transitions: Transitions = HashMap::new();
+    let mut transitions = Transitions::new();
     for line in iter {
         if line.is_empty() {
             continue;
@@ -25,6 +25,7 @@ fn parse_input(data: &str) -> (Vec<char>, Transitions) {
     (starting, transitions)
 }
 
+#[allow(dead_code)]
 fn debug_counts(prefix: &str, counts: &Counts) {
     println!("{}", prefix);
     counts.iter().filter(|x| {
@@ -35,7 +36,7 @@ fn debug_counts(prefix: &str, counts: &Counts) {
 }
 
 fn solve(steps: usize, (starting, transitions): &(Vec<char>, Transitions)) -> Res {
-    let mut counts: Counts = HashMap::new();
+    let mut counts = Counts::new();
     for (&key ,_) in transitions.iter() {
         counts.insert(key, 0);
     }
@@ -56,34 +57,24 @@ fn solve(steps: usize, (starting, transitions): &(Vec<char>, Transitions)) -> Re
                 continue;
             }
             if let Some(&ch) = transitions.get(key) {
-                let entry = next_counts.entry((key.0, key.1)).or_insert(cnt);
-                *entry -= cnt;
-                // dbg!("removing", (key.0, key.1));
-                let entry = next_counts.entry((key.0, ch)).or_insert(0);
-                *entry += cnt;
-                let entry = next_counts.entry((ch, key.1)).or_insert(0);
-                *entry += cnt;
-                // dbg!("adding", (key.0, ch), (ch, key.1));
+                *next_counts.entry((key.0, key.1)).or_insert(cnt) -= cnt;
+                *next_counts.entry((key.0, ch)).or_insert(0) += cnt;
+                *next_counts.entry((ch, key.1)).or_insert(0) += cnt;
             };
         }
         // debug_counts("out:", &next_counts);
         counts = next_counts;
     }
 
-    let mut letter_counts : HashMap<char, usize> = HashMap::new();
+    let mut letter_counts: HashMap<char, usize> = HashMap::new();
     for (key, val) in counts.iter() {
-        let entry = letter_counts.entry(key.0).or_insert(0);
-        *entry += val;
+        *letter_counts.entry(key.0).or_insert(0) += val;
     }
-    let entry = letter_counts.entry(*last_letter).or_insert(0);
-    *entry += 1;
-
-    // dbg!(&letter_counts);
+    *letter_counts.entry(*last_letter).or_insert(0) += 1;
 
     let mut r = letter_counts.iter().map(|x| *x.1).collect::<Vec<usize>>();
     r.sort_unstable();
-    // dbg!(r);
-    
+
     r.last().unwrap() - r.first().unwrap()
 }
 
